4861c1c14940abc1b2f58819ae2cdb3270467ee5
leMaik <maik@maik.tk>
2016-02-11 12:33:42 +0100

    Add project files and the first plugin and api classes.

---
+++ src/main/java/de/craften/plugins/minigamemanager/MinigameManagerPlugin.java
@@ -0,0 +1,12 @@
+package de.craften.plugins.minigamemanager;
+
+import de.craften.plugins.minigamemanager.api.MinigameApi;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The MinigameManager plugin that manages scoreboard and configuration.
+ * <p/>
+ * If you want to integrate your minigame, look into {@link MinigameApi}.
+ */
+public class MinigameManagerPlugin extends JavaPlugin {
+}
